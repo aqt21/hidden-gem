8979a650281288d2d0a31c2f6f54e892608cbda9
Constance La <you@example.com>
2018-05-04 07:32:56 -0700

    Edited footer

--- src/App.js
+++ src/App.js
@@ -94,18 +94,15 @@ var App = React.createClass({
 								<div className="col l4 offset-l2 s12">
 									<h5 className="white-text">Links</h5>
 									<ul>
-										<li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-										<li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-										<li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-										<li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+										<li><a className="grey-text text-lighten-3" href="#!">About Us</a></li>
+
 									</ul>
 								</div>
 							</div>
 						</div>
 						<div className="footer-copyright">
 							<div className="container">
-							© 2014 Copyright Text
-							<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+							© 2018 Copyright Text
 							</div>
 						</div>
 					</footer>
